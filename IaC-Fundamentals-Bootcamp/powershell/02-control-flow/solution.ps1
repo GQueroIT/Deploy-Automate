3293a1beba898946564e852ea7705b7597398f04
GQueroIT <gquero52@outlook.com>
2026-08-21 00:09:27 -0400

    Powershell 02-Control Flow solution

--- IaC-Fundamentals-Bootcamp/powershell/02-control-flow/solution.ps1
+++ IaC-Fundamentals-Bootcamp/powershell/02-control-flow/solution.ps1
@@ -1,1 +1,28 @@
 # Solution - write your work here
+$servers = @(
+  @{ ServerName = "Server01"; Status = "UP" }
+  @{ ServerName = "Server02"; Status = "Down" }
+  @{ ServerName = "Server03"; Status = "Down" }
+  @{ ServerName = "Server04"; Status = "Up" }
+  @{ ServerName = "Server05"; Status = "Degraded" }
+  @{ ServerName = "Server06"; Status = "Maintenance" }
+  @{ ServerName = "Server07"; Status = "monkeys" }
+)
+
+foreach ($server in $servers) {
+  if ($server.Status -eq "Down") {
+    Write-Host "$($server.ServerName) is down"
+  }
+  elseif ($server.Status -eq "Degraded") {
+    Write-Host "$($server.ServerName) is degraded"
+  }
+  elseif ($server.Status -eq "Up") {
+    Write-Host "$($server.ServerName) is Up"
+  }
+  elseif ($server.Status -eq "Maintenance") {
+    Write-Host "$($server.ServerName) is under maintenance"
+  }
+  else {
+    Write-Host "$($server.ServerName) has an unknown status: $($server.Status)"
+  }
+}
